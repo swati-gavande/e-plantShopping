171133a58e770130eb1bbfe8976b5f4544382e33
swati.gavande <swati.gavande94@gmail.com>
2026-01-28 08:59:09 -0500

    test

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -10,7 +10,7 @@ function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState([]);
-console.log(addedToCart)
+    console.log(addedToCart)
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -265,7 +265,7 @@ console.log(addedToCart)
         //     ...prevState, // Spread the previous state to retain existing entries
         //     [product.name]:true, // Set the current product's name as a key with value 'true' to mark it as added
         // }));
-        setAddedToCart(prevItems => [...prevItems, product.na]);
+        setAddedToCart(prevItems => [...prevItems, product.name]);
     };
 
     const calculateTotalQuantity = () => {
@@ -289,7 +289,7 @@ console.log(addedToCart)
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill={calculateTotalQuantity()}></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none">1</rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
                 </div>
             </div>
             {!showCart ? (
@@ -306,14 +306,14 @@ console.log(addedToCart)
                                         <div className="product-title">{plant.name}</div>
                                         <div className="product-description">{plant.description}</div> {/* Display plant description */}
                                         <div className="product-cost">{plant.cost}</div> {/* Display plant cost */}
-                                        <button
+                                        {!addedToCart.includes(plant.name) ? (<button
                                             className="product-button"
                                             onClick={() => handleAddToCart(plant)} // Handle adding plant to cart
                                         >
                                             Add to Cart
-                                        </button> 
-                                            {/* <button className='.product-button.added-to-cart'>Added to Cart</button> */}
-                                     
+                                        </button>) :
+                                            (<button className='.product-button.added-to-cart'>Added to Cart</button>)}
+
                                     </div>
                                 ))}
                             </div>
